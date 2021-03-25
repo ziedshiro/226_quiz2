5a83cdc486dc307191f17a908a1603117b8d3aab
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-25 19:14:17 +0700

    2

--- 2.c
+++ 2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n,m;
+    int i,j,n,m,z,X,Y,Z;
     scanf("%d %d",&m,&n);
     char s[m][70];
     for(i=0;i<m;i++)
@@ -11,9 +11,23 @@ int main()
     for(i=0;i<n;i++)
         for(j=0;j<3;j++)
         scanf("%d",&x[i][j]);
-    for(i=0;i<n;i++)
-        for(j=0;j<3;j++)
-            printf("%d",x[i][j]);
+        for(j=0;j<n;j++)
+        {
+            X=x[j][0]-1;
+            Z=x[j][2];
+            for(z=0;z<Z;z++)
+            {
+                Y=x[j][1]-1+z;
+                s[X][Y]='x';
+            }
+        }
+        for(i=0;i<m;i++)
+        {
+           for(j=0;j<70;j++)
+            printf("%c",s[i][j]);
+            printf("\n");
+        }
+
 
 
 }
