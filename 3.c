a4f74eca372081cf5db94a566a16dee65b00ae39
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-25 19:30:51 +0700

    3

--- 3.c
+++ 3.c
@@ -1,15 +1,29 @@
 #include<stdio.h>
 int number(int n)
 {
-    int N,num[100],i=0;
+    int k,N,num[100]={},i=0,mun[100]={},r,sum=0,ans=0;
     N=n;
     do
     {
         num[i]=N%10;
         N/=10;
         i++;
-
     }while(N>0);
+    r=i;
+    for(k=0;k<r;k++)
+    {
+        mun[k]=num[i-1];
+        i--;
+    }
+    for(i=0;i<r;i++)
+    {
+        if(mun[i]==num[i])
+            sum++;
+            //printf("%d_%d",num[i],mun[i]);
+    }
+    if(r==sum)
+        ans=1;
+    return ans;
 
 }
 int main()
@@ -23,7 +37,7 @@ int main()
 
         for(i=10;i<=n;i++)
         {
-            number(i);
+            sum+=number(i);
         }
         printf("%d",sum);
     }
