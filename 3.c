e9a12ac9a09c75c60a426d9ff9fa0a78c87c096e
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-25 19:21:43 +0700

    3

--- 3.c
+++ 3.c
@@ -1,55 +1,29 @@
 #include<stdio.h>
-int len(int n)
+int number(int n)
 {
-    int N,i=0;
+    int N,num[100],i=0;
     N=n;
     do
     {
+        num[i]=N%10;
         N/=10;
         i++;
-    }while(N>0);
-    return i;
-}
-int pow(int i)
-{
-    int n,x;
-    n=1,x;
-    for(x=0;x<i-1;x++)
-        n*=10;
-        return n;
-}
-int pd(int n,int sum)
-{
-    int N,i,pd=0,s,pw,x=0;
-    i=len(n);
-    N=n;
-    pw=pow(i);
 
-    do
-    {
-        s=(N%10);
-        pd=pd+(s*pw);
-        N/=10;
-        pw/=10;
-        x++;
-    }while(x!=i);
-    if(pd==n)
-        sum=1;
-    else
-        sum=0;
-    return sum;
+    }while(N>0);
+
 }
 int main()
 {
-    int n,i,sum=9,x;
+    int n,i=0,sum=9,x,N;
     scanf("%d",&n);
     if(n<10)
         printf("%d",n);
     else
     {
+
         for(i=10;i<=n;i++)
         {
-            sum+=pd(i,x);
+            number(i);
         }
         printf("%d",sum);
     }
