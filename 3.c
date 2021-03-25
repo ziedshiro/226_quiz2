6335e679902c6bbfa6ec08ce8522363339c93f22
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-25 18:00:19 +0700

    3

--- 3.c
+++ 3.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,sum=0;
+    int n,i,sum=9;
     scanf("%d",&n);
     if(n<10)
         printf("%d",n);
+    else
+    {
+        for(i=10;i<=n;i++)
+        {
+            printf("%d",sum);
+        }
+    }
 }
