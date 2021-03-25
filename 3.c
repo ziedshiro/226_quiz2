af4baf5389f9e9772bc6c88d9ee52c1231a2eec1
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-25 18:04:53 +0700

    3

--- 3.c
+++ 3.c
@@ -1,4 +1,29 @@
 #include<stdio.h>
+#include<math.h>
+int len(int n)
+{
+    int N,i=0;
+    N=n;
+    do
+    {
+        N/10;
+        i++;
+    }while(N>0);
+    return i;
+}
+int pd(int n,int sum)
+{
+    int N,i,pd,s=0,pw;
+    i=len(n);
+    N=n;
+    pw=pow(10,i);
+    do
+    {
+        s=(N%10);
+        N/=10;
+        s++;
+    }while(s!=i);
+}
 int main()
 {
     int n,i,sum=9;
@@ -9,7 +34,8 @@ int main()
     {
         for(i=10;i<=n;i++)
         {
-            printf("%d",sum);
+            pd(i,sum);
         }
+        printf("%d",sum);
     }
 }
