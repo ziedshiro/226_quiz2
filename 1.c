0b824b2340f2f6f1996d495cb146e26e1c35225e
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-25 17:46:29 +0700

    1

--- 1.c
+++ 1.c
@@ -1,8 +1,36 @@
 #include<stdio.h>
+int ans(int sum,int N)
+{
+    int i,k,n,j=0;
+    for(i=2;i<=N;i++)
+    {
+        k=2;
+        n=i;
+        j=0;
+        do
+        {
+           if(n%k==0)
+        {
+            n/=k;
+            j++;
+        }
+        else
+        {
+            k++;
+            if(j<3)
+                j=0;
+        }
+        }while(n>1);
+        if(j<3)
+            sum++;
+    }
+    return sum;
+}
 int main()
 {
-    int n,i=2,sum=0,j=0;
+    int n,i=2,sum=1,j=0,N;
     scanf("%d",&n);
+    N=n;
     do
     {
         if(n%i==0)
@@ -18,11 +46,12 @@ int main()
         }
     }while(n>1);
     if(j>=3)
-    {
-         printf("Not Cube Free");;
-    }
+         printf("Not Cube Free");
     else
     {
-        printf("y");;
+        if(N==1)
+            printf("1");
+        else
+        printf("%d",ans(sum,N));;
     }
 }
