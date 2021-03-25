e1fe25b5bf2368a507bcf3190bb163d0c10a2165
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-25 17:08:52 +0700

    1

--- 1.c
+++ 1.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,i=0;
+    int n,i=1,j=0;
     scanf("%d",&n);
     do
     {
+        if(n%i==0)
+            j++;
+        else
+        {
+            j=0;
+            i++;
+        }
 
     }while(i!=n);
+    if(j==3)
+        printf("y");
+    else
+        printf("Not Cube Free");
 }
